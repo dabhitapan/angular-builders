9e90f0c121890102076fb27a8210a361d9963950
JeB <work@justjeb.com>
2022-06-05 16:29:20 +0300

    chore: add separate execution for clean Jest

--- packages/jest/src/jest-config/default-config.ts
+++ packages/jest/src/jest-config/default-config.ts
@@ -1,13 +1,19 @@
-import { globals } from 'jest-preset-angular/jest-preset.js';
+import * as config from 'jest-preset-angular/jest-preset.js';
 
 // eslint-disable-next-line no-undef
 globalThis.ngJest = {
   skipNgcc: false,
-  tsconfig: 'tsconfig.spec.json',
 };
 
+console.log(JSON.stringify(config, null, 2));
+
 export default {
-  globals,
+  globals: {
+    'ts-jest': {
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
   preset: 'jest-preset-angular',
   testEnvironmentOptions: {
     url: 'https://github.com/just-jeb/angular-builders',
@@ -17,4 +23,5 @@ export default {
     '\\.(jpg|jpeg|png)$': `${__dirname}/mock-module.js`,
   },
   globalSetup: 'jest-preset-angular/global-setup',
+  snapshotSerializers: config.snapshotSerializers,
 };
